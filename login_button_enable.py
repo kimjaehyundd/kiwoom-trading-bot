d2d8704e1fb7a31aff94a54aa04f89f0546df67b
kimjaehyundd <152826041+kimjaehyundd@users.noreply.github.com>
2025-06-27 02:47:24 +0900

    feat: 로그인시 조건식 버튼 활성화 추가

---
+++ login_button_enable.py
@@ -0,0 +1,4 @@
+                self.logout_button.setEnabled(True)
+                self.add_stock_button.setEnabled(True)
+                self.remove_stock_button.setEnabled(True)
+                self.load_condition_button.setEnabled(True)
